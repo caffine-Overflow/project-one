dda8d6dee4df2519608ad57cef8aeee224e514c8
caffine_Overflow <tnzajk@gmail.com>
2024-10-05 18:52:50 +0530

    formatting date done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
   const expenses = [
-    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 , location: "Bangalore" },
-    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 ,location: "Delhi" },
-    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 , location:"Hyderabad" },
-    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 , location:"Mumbai"},
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
   ];
 
   return (
@@ -18,7 +18,7 @@ function App() {
             date={expense.date}
             title={expense.title}
             price={expense.price}
-            location={expense.location}
+            
           ></ExpenseItem>
         );
       })}
--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,10 +1,24 @@
 import "./ExpenseItem.css";
 
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
 function ExpenseItem(props) {
+
+    const month = months[props.date.getMonth()];
+    const year = props.date.getFullYear();
+    // to  Ensure day has leading zero if needed
+    const day = props.date.getDate().toString().padStart(2, '0');
+  
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__location">{props.location}</div>
+        <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+        </div>
+        
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
