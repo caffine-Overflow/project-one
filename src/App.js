c4012e3480c92b9b18ccb97d4e7eb16c461f99d5
caffine_Overflow <tnzajk@gmail.com>
2024-10-05 18:44:37 +0530

    reusable components creaated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 , location: "Bangalore" },
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 ,location: "Delhi" },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 , location:"Hyderabad" },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 , location:"Mumbai"},
+  ];
+
   return (
-   <div>
-    <h1>Let's get Started</h1>
-    <ExpenseItem
-        date={new Date(2023, 7, 15)}
-        location="Bangalore"
-        title="Insurance"
-        price="50"
-      ></ExpenseItem>
-       <ExpenseItem
-        date={new Date(2023, 3, 25)}
-        location="Delhi"
-        title="Book"
-        price="20"
-      ></ExpenseItem>
-       <ExpenseItem
-        date={new Date(2023, 10, 11)}
-        location="Hyderabad"
-        title="Pen"
-        price="5"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 1, 14)}
-        location="Mumbai"
-        title="Laptop"
-        price="200"
-      ></ExpenseItem>
-      <ExpenseItem
-    date={new Date(2022,12,3)}
-    location="London"
-    title="Test"
-    price="50"
-    ></ExpenseItem>
-   </div>
+    <div>
+      <h1>Let's get Started</h1>
+      {expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+            location={expense.location}
+          ></ExpenseItem>
+        );
+      })}
+    </div>
   );
 }
 
