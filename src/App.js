05cd7cab0ef8776bd179ffc2cc5ba513324dcc58
caffine_Overflow <tnzajk@gmail.com>
2024-10-05 16:36:33 +0530

    import custom component tp parent component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
   return (
    <div>
     <h1>Let's get Started</h1>
-    <p>I am learning React</p>
+    <ExpenseItem></ExpenseItem>
    </div>
   );
 }
