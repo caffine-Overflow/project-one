d00a1782d4b922ff7523729e39c57eec03040dd2
caffine_Overflow <tnzajk@gmail.com>
2024-10-05 16:35:30 +0530

    ExpenseItem custom component created

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <>
+           <h2>Food - $5</h2>
+           <h2>Petrol - $20</h2>       
+           <h2>Movie - $10</h2>
+
+        </>
+    )
+}
+export default ExpenseItem;
