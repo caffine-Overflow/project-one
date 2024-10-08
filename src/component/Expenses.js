eba10e2892cf26f78f809db909d3e1057b1d6a38
caffine_Overflow <tnzajk@gmail.com>
2024-10-08 13:43:59 +0530

    Added reusable Card component and integreated it into Expenses, and ExpenseItem

--- src/component/Expenses.js
+++ src/component/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css"
+import Card from "./Card";
 function Expenses(props){
   return(
-    <div className="expenses">
+    <Card className="expenses">
        {props.expenses.map((expense, index) => {
         return (
           <ExpenseItem
@@ -10,10 +11,10 @@ function Expenses(props){
             date={expense.date}
             title={expense.title}
             price={expense.price}
-          ></ExpenseItem>
+          />
         );
       })}
-    </div>
+    </Card>
   );
 }
 export default Expenses;
