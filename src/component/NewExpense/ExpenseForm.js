e78f0a4f62251c8f90a4125190c0ff5c2efbab08
caffine_Overflow <tnzajk@gmail.com>
2024-10-09 21:01:19 +0530

    listening for user input , using useState Hook

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -6,6 +6,7 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    //event.target gives input element
   };
 
   const [enteredAmount, setEnteredAmount] = useState("");
@@ -22,16 +23,21 @@ const ExpenseForm = () => {
     <form>
       <div className=".new-expense__controls">
         <div className=".new-expense__control">
-          <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" onChange={titleChangeHandler} />
         </div>
         <div className=".new-expense__control">
-          <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler}></input>
-        </div>
-        <div className=".new-expense__control">
-          <label>Date</label>
+          <label htmlFor="amount">Amount</label>
           <input
+            type="number"
+            id="amount"
+            onChange={amountChangeHandler}
+          ></input>
+        </div>
+        <div className=".new-expense__control">
+          <label htmlFor="date">Date</label>
+          <input
+            id="date"
             type="date"
             min="2023-01-01"
             max="2024-12-31"
